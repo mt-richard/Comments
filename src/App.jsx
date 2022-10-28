e7e1f7412786b058436d6a41b2991c968bf19d4b
Richard <87782510+mt-richard@users.noreply.github.com>
2022-10-28 14:57:28 +0000

    my commit again

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import { useState } from 'react';
 export default function App() {
 
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
-  
-  function saveComment(e){
+  function onSend(e) {
     e.preventDefault();
     setComments([...comments, comment]);
     // alert(comment);
-    setComment();
+    setComment("");
   }
+
   return (
     <main className="container m-5">
-      <form className="form-control ">
-        <textarea value={comment} placeholder="Leave a comment" className="form-control mb-3" onChange={(e) => setComment(e.target.value)} >
+      <form className="">
+        <textarea value={comment} placeholder="Leave a comment" 
+          className="form-control mb-3" 
+          onChange={(e) => setComment(e.target.value)} >
         </textarea>
-        <button onClick={saveComment} type="button" className="btn btn-primary">Send</button>
+        <button onClick={onSend} type="button" className="btn btn-primary">Send</button>
       </form>
+
       <ul>
-        {comments.map((item, id) => (
-            <li key={id}>{item}</li>
-        ))}
-         {/*<li>{comment}</li>*/}
+        {comments.map((item, id) => (<li key={id}>{item}</li>))}
       </ul>
-      
     </main>
-  )
+  );
 }
