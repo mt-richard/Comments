3e60533d5cfc311d264c2c3c3e8bb31502f7df6d
Richard <87782510+mt-richard@users.noreply.github.com>
2022-10-28 14:28:36 +0000

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import './App.css'
-
+import { useState } from 'react';
 export default function App() {
+
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+  
+  function saveComment(e){
+    e.preventDefault();
+    setComments([...comments, comment]);
+    // alert(comment);
+    setComment();
+  }
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+    <main className="container m-5">
+      <form className="form-control ">
+        <textarea value={comment} placeholder="Leave a comment" className="form-control mb-3" onChange={(e) => setComment(e.target.value)} >
+        </textarea>
+        <button onClick={saveComment} type="button" className="btn btn-primary">Send</button>
+      </form>
+      <ul>
+        {comments.map((item, id) => (
+            <li key={id}>{item}</li>
+        ))}
+         {/*<li>{comment}</li>*/}
+      </ul>
+      
     </main>
   )
 }
